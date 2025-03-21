9bb0b8d389903096f9d0ed33c893fe21deb8461f
Ulysse BOUDIGUES <uboudigu@paul-f4Br6s1.clusters.42paris.fr>
2025-03-21 17:20:58 +0100

    re-update

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -3,7 +3,7 @@
 service mariadb start
 mariadb -e "CREATE DATABASE IF DOES NOT EXIST ${INCEPTION_MYSQL_DATATBASE}"
 mariadb -e "CREATE USER IF DOES NOT EXIST '${INCEPTION_MYSQL_USER}'@'%' IDENTIFIED BY '${INCEPTION_MYSQL_PASS}'"
-mariadb -e "GRANT ALL ON ${INCEPTION_MYSQL_DATABASE}.* TO '${INCEPTION_MY_SQL_USER}'@'%';"
+mariadb -e "GRANT ALL ON ${INCEPTION_MYSQL_DATABASE}.* TO '${INCEPTION_MYSQL_USER}'@'%';"
 mariadb -e "FLUSH PRIVILEGES;"
 mysqladmin shutdown -u root
 mysqld --bind-adress=0.0.0.0 --port=3306 --user=root

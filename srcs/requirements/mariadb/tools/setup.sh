2486963bae71d1577c076432bd243266bdc1bde6
uboudigu <ulysseboudigues@gmail.com>
2025-03-26 00:50:59 +0100

    Error establishing a database connection

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 service mariadb start
-mariadb -e "CREATE DATABASE IF DOES NOT EXIST ${INCEPTION_MYSQL_DATATBASE}"
-mariadb -e "CREATE USER IF DOES NOT EXIST '${INCEPTION_MYSQL_USER}'@'%' IDENTIFIED BY '${INCEPTION_MYSQL_PASS}'"
+mariadb -e "CREATE DATABASE IF NOT EXISTS ${INCEPTION_MYSQL_DATABASE}"
+mariadb -e "CREATE USER IF NOT EXISTS '${INCEPTION_MYSQL_USER}'@'%' IDENTIFIED BY '${INCEPTION_MYSQL_PASS}'"
 mariadb -e "GRANT ALL ON ${INCEPTION_MYSQL_DATABASE}.* TO '${INCEPTION_MY_SQL_USER}'@'%';"
 mariadb -e "FLUSH PRIVILEGES;"
 mysqladmin shutdown -u root
-mysqld --bind-adress=0.0.0.0 --port=3306 --user=root
+mysqld --bind-address=0.0.0.0 --port=3306 --user=root
